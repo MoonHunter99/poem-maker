26ee7dd71c95a1f094b155cf76e96c916c45c5cd
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-28 21:13:51 +0800

    Pseudocode

---
+++ peom maker.py
@@ -0,0 +1,8 @@
+#make a list for the yes option in asking the user again
+#make the list for the no option in asking the user again
+#open a file to make a text file called mylife.txt
+    #create a loop to ask what to put in the file
+        #ask the user for the input in the file
+        #write the input of the user in the file
+        #ask the user if he/she wants to put more lines
+        #check the answer in the ask user again
