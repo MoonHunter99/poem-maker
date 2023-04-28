f43dce6dae87cb0be7600a75f8df5acfffe14bfd
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-28 21:16:59 +0800

    asking the user for line input working and loop created

--- peom maker.py
+++ peom maker.py
@@ -3,8 +3,11 @@ yes_user_input = ["Y" , "y" , "Yes" , "YES"]
 #make the list for the no option in asking the user again
 no_user_input = ["N" , "n" , "No" , "NO"]
 #open a file to make a text file called mylife.txt
+with open("mylife.txt" , "a") as mylife_file:
     #create a loop to ask what to put in the file
+    while True:
         #ask the user for the input in the file
+        user_line_input = str(input("Enter a line to add to your file: "))
         #write the input of the user in the file
         #ask the user if he/she wants to put more lines
         #check the answer in the ask user again
