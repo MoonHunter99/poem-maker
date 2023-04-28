0df630f1bbc3dfba3f648ba104d4b346617e2f96
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-04-28 21:14:48 +0800

    lists for options are created

--- peom maker.py
+++ peom maker.py
@@ -1,5 +1,7 @@
 #make a list for the yes option in asking the user again
+yes_user_input = ["Y" , "y" , "Yes" , "YES"]
 #make the list for the no option in asking the user again
+no_user_input = ["N" , "n" , "No" , "NO"]
 #open a file to make a text file called mylife.txt
     #create a loop to ask what to put in the file
         #ask the user for the input in the file
